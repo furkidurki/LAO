8354faf7e284868f92f2dba4280a1f32a3d9c0fa
fural <sutepuoffical@gmail.com>
2025-12-12 14:30:49 +0100

    added magazzino

---
+++ LAO/app/(tabs)/magazzino.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MagazzinoScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Magazzino</Text>
+        </View>
+    );
+}
