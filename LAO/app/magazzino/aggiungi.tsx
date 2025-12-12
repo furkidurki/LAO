e7578a51cdb307b7b10c5c787c58fc47de02f436
fural <sutepuoffical@gmail.com>
2025-12-12 15:14:55 +0100

    sistemando il forum di aggiungi e diverse altere cose

--- LAO/app/magazzino/aggiungi.tsx
+++ LAO/app/magazzino/aggiungi.tsx
@@ -1,9 +1,27 @@
-import { View, Text } from "react-native";
-
+import {View, Text, TextInput} from "react-native";
+import {useState} from "react";
 export default function AggiungiMagazzino() {
+const [nomeMateriale, setNomeMateria] = useState("");
+const [quantita, setQuantita] = useState("");
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Aggiungi elemento al magazzino</Text>
-        </View>
+       <View style={{ flex: 1 }}>
+           <View> <Text>aggiungi elemento nel maggazzino</Text>
+               <TextInput
+                   value={nomeMateriale}
+                   onChangeText={setNomeMateria}
+                   placeholder="nome materiale"/>
+
+               <Text>hai inserito {nomeMateriale}</Text> </View>
+
+           <View>
+               <Text>aggiungi quantita</Text>
+               <TextInput
+                   value={quantita}
+                   onChangeText={setQuantita}
+                   placeholder="quantita"/>
+           </View>
+
+       </View>
+
     );
 }
